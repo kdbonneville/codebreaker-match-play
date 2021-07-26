a2c84e5c9347f3856db7a6fa811b5f5a0ca71746
Kelley <kbonneville@cnm.edu>
2021-07-26 10:11:15 -0600

    created SpringData repository interfaces. pt 2

--- src/main/java/edu/cnm/deepdive/codebreaker/model/dao/GuessRepository.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/dao/GuessRepository.java
@@ -2,13 +2,14 @@ package edu.cnm.deepdive.codebreaker.model.dao;
 
 import edu.cnm.deepdive.codebreaker.model.entity.Code;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
+import edu.cnm.deepdive.codebreaker.model.entity.User;
 import java.util.UUID;
 import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
+
 
 public interface GuessRepository extends JpaRepository<Guess, UUID> {
 
-  Stream<Guess> findAllByUserAndCodeByCreatedAsc(User user, Code code);
+  Stream<Guess> findAllByUserAndCodeOrderByCreatedAsc(User user, Code code);
 
 }
